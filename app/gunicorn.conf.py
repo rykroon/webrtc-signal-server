c090048b94cd2e3889c7c9ac2af0a61dfb3020b6
Ryan Kroon <rykroon.tech@gmail.com>
2022-10-29 17:54:19 -0400

    Updated default number of workers to 1.

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,14 +1,12 @@
-import os
 from starlette.config import Config
 
 
 _config = Config()
-_cpu_count = os.cpu_count()
 
 
 bind            = _config.get('GUNICORN_BIND', default='0.0.0.0')
 loglevel        = _config.get('GUNICORN_LOGLEVEL', default='info')
 preload_app     = _config.get('GUNICORN_PRELOAD_APP', cast=bool, default=True)
 reload          = _config.get('GUNICORN_RELOAD', cast=bool, default=False)
-workers         = _config.get('GUNICORN_WORKERS', cast=int, default=2 * _cpu_count + 1)
+workers         = _config.get('GUNICORN_WORKERS', cast=int, default=1)
 worker_class    = _config.get('GUNICORN_WORKER_CLASS', default='uvicorn.workers.UvicornWorker')
