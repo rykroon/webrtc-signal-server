e9eab715a479a4492b18cf22fd3aef445aa4da5e
Ryan Kroon <rykroon.tech@gmail.com>
2022-11-01 17:53:15 -0400

    created new websocket endpoint.

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ from starlette.applications import Starlette
 from starlette.routing import Route, WebSocketRoute, Mount
 from starlette.staticfiles import StaticFiles
 
-from views import Homepage, Websocket
+from views import Homepage, Websocket, RandomChatWS
 from utils import Cache
 
 
 routes = [
     Route('/', Homepage),
+    WebSocketRoute('/ws2', RandomChatWS),
     WebSocketRoute('/ws', Websocket),
     Mount('/static', app=StaticFiles(directory='static'), name='static')
 ]
